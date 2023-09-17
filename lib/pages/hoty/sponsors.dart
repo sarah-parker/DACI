4a444aac720b8dde2c92a4a2d068854ad152a648
Sarah Parker <sdkay88@gmail.com>
2023-09-18 07:10:15 +1000

    add sponsors

--- lib/pages/hoty/sponsors.dart
+++ lib/pages/hoty/sponsors.dart
@@ -15,54 +15,66 @@ class HotySponsors extends StatelessWidget {
       } else {}
     }
 
-    return Padding(
-      padding: EdgeInsets.symmetric(
-          vertical: 25.0, horizontal: screenSize.width * 0.05),
-      child: Column(
-        children: [
-          Image.asset(
-            'images/cropped-ico.png',
-            height: 250,
-          ),
-          Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Text(
-                "Thank you for your entry to Qld Arabian Horse of the Year. \nWe're thrilled to have you as part of our prestigious event!",
-                style: Theme.of(context).textTheme.displayMedium,
-                textAlign: TextAlign.center,
-              )),
-          const Padding(
-            padding: EdgeInsets.all(12.0),
-            child: Text(
-              """As you prepare for the competition, we invite you to explore and support our generous sponsors, whose contributions make this event possible. \n
-Click below to learn more about them and how they enhance our equestrian community.""",
-              textAlign: TextAlign.center,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 25.0, horizontal: screenSize.width * 0.05),
+        child: Column(
+          children: [
+            Image.asset(
+              'images/cropped-ico.png',
+              height: 250,
             ),
-          ),
-          Wrap(
-            spacing: 15,
-            children: [
-              MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: GestureDetector(
-                      onTap: () =>
-                          launchURL('https://toowoombagrooming.com.au/'),
-                      child: Image.asset(
-                        'images/toowoomba-grooming.png',
-                        height: 250,
-                      ))),
-              // MouseRegion(
-              //     cursor: SystemMouseCursors.click,
-              //     child: GestureDetector(
-              //         onTap: () =>
-              //             _launchURL('https://toowoombagrooming.com.au/'),
-              //         child: Image.asset(
-              //           'images/toowoomba-grooming.png',
-              //           height: 250,
-              //         )))
-            ],
-          ),
-        ],
+            Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Text(
+                  "Thank you for your entry to Qld Arabian Horse of the Year. \nWe're thrilled to have you as part of our prestigious event!",
+                  style: Theme.of(context).textTheme.displayMedium,
+                  textAlign: TextAlign.center,
+                )),
+            const Padding(
+              padding: EdgeInsets.all(12.0),
+              child: Text(
+                """As you prepare for the competition, we invite you to explore and support our generous sponsors, whose contributions make this event possible. \n
+      Click below to learn more about them and how they enhance our equestrian community.""",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Wrap(
+              spacing: 15,
+              runSpacing: 15,
+              children: [
+                Column(
+                  children: [
+                    Text("• Carlyn Brennan",
+                        style: Theme.of(context).textTheme.displayMedium),
+                    const Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+                    Text("• Hoofprintz Photography",
+                        style: Theme.of(context).textTheme.displayMedium),
+                  ],
+                ),
+                MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                        onTap: () =>
+                            launchURL('https://toowoombagrooming.com.au/'),
+                        child: Image.asset(
+                          'images/toowoomba-grooming.png',
+                          height: 250,
+                        ))),
+                MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                        onTap: () => launchURL(
+                            'https://www.facebook.com/horseland.goldcoast'),
+                        child: Image.asset(
+                          'images/Horseland-GC.jpg',
+                          height: 250,
+                        )))
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/totr/sponsors.dart
+++ lib/pages/totr/sponsors.dart
@@ -15,53 +15,62 @@ class TOTRSponsors extends StatelessWidget {
       } else {}
     }
 
-    return Padding(
-      padding: EdgeInsets.symmetric(
-          vertical: 25.0, horizontal: screenSize.width * 0.05),
-      child: Column(
-        children: [
-          Image.asset(
-            'images/cropped-ico.png',
-            height: 250,
-          ),
-          Padding(
-              padding: const EdgeInsets.all(18.0),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: 25.0, horizontal: screenSize.width * 0.05),
+        child: Column(
+          children: [
+            Image.asset(
+              'images/cropped-ico.png',
+              height: 250,
+            ),
+            Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Text(
+                  "Thank you for your entry to Top of the Range Arabian Event. \nWe're excited to have you join us for this incredible event!",
+                  style: Theme.of(context).textTheme.displayMedium,
+                  textAlign: TextAlign.center,
+                )),
+            const Padding(
+              padding: EdgeInsets.all(12.0),
               child: Text(
-                "Thank you for your entry to Top of the Range Arabian Event. \nWe're excited to have you join us for this incredible event!",
-                style: Theme.of(context).textTheme.displayMedium,
-                textAlign: TextAlign.center,
-              )),
-          const Padding(
-            padding: EdgeInsets.all(12.0),
-            child: Text(
-                """As we gear up for the competition, we'd like to express our gratitude to our dedicated sponsors. They play a vital role in making this show a success. \n
-Please take a moment to discover our sponsors' offerings by clicking below. Your support of their businesses is a valuable part of our equestrian community.""",
-                textAlign: TextAlign.center),
-          ),
-          Wrap(
-            spacing: 15,
-            children: [
-              MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: GestureDetector(
-                      onTap: () =>
-                          launchURL('https://toowoombagrooming.com.au/'),
-                      child: Image.asset(
-                        'images/toowoomba-grooming.png',
-                        height: 250,
-                      ))),
-              // MouseRegion(
-              //     cursor: SystemMouseCursors.click,
-              //     child: GestureDetector(
-              //         onTap: () =>
-              //             _launchURL('https://toowoombagrooming.com.au/'),
-              //         child: Image.asset(
-              //           'images/toowoomba-grooming.png',
-              //           height: 250,
-              //         )))
-            ],
-          ),
-        ],
+                  """As we gear up for the competition, we'd like to express our gratitude to our dedicated sponsors. They play a vital role in making this show a success. \n
+      Please take a moment to discover our sponsors' offerings by clicking below. Your support of their businesses is a valuable part of our equestrian community.""",
+                  textAlign: TextAlign.center),
+            ),
+            Wrap(
+              spacing: 15,
+              runSpacing: 15,
+              children: [
+                // Column(
+                //   children: [
+                //     Text("• Carlyn Brennan",
+                //         style: Theme.of(context).textTheme.displayMedium),
+                //   ],
+                // ),
+                MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                        onTap: () =>
+                            launchURL('https://toowoombagrooming.com.au/'),
+                        child: Image.asset(
+                          'images/toowoomba-grooming.png',
+                          height: 250,
+                        ))),
+                // MouseRegion(
+                //     cursor: SystemMouseCursors.click,
+                //     child: GestureDetector(
+                //         onTap: () => launchURL(
+                //             'https://www.facebook.com/horseland.goldcoast'),
+                //         child: Image.asset(
+                //           'images/Horseland-GC.jpg',
+                //           height: 250,
+                //         )))
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
